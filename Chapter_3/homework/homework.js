977cba012d5d98652097904efca51e8ca2440966
ThienProFE <100427429+ThienProFE@users.noreply.github.com>
2022-07-11 14:05:18 +0700

    update
    update

--- Chapter_3/homework/homework.js
+++ Chapter_3/homework/homework.js
@@ -17,7 +17,7 @@ const server = http.createServer((req, res) => {
   } else if (req.url === "/about") {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(
-      "<h1>${student}</h1>"
+      `<h1>${student}</h1>`
     );
     res.end();
   } else {
